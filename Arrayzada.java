03b723813aabb37feddba4ef111fc81bee0aefb5
DanielPRodrigues <40270607+DanielPRodrigues@users.noreply.github.com>
2021-09-08 19:08:37 -0300

    Update Arrayzada.java

--- Arrayzada.java
+++ Arrayzada.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 
-//criando Array
 class ArrayCompartilhado {
    private int[] array;
 
 
    public ArrayCompartilhado(int length) {
       this.array = new int[length];
-      Random rnd = new Random();
+      Random random = new Random();
       for (int i = 0; i < length; i++ ) {
-         this.array[i] = rnd.nextInt(10 - 1) + 1;
+         this.array[i] = random.nextInt(100 - 1) + 1;
       }
    }
 
@@ -27,7 +26,7 @@ class T extends Thread {
 
    int NThreads;
 
-   int quantEven = 0;
+   int quantPares = 0;
 
    public T(int tid, ArrayCompartilhado a, int n) {
       this.id = tid;
@@ -35,13 +34,13 @@ class T extends Thread {
       this.NThreads = n;
    }
    public int getEven(){
-      return this.quantEven;
+      return this.quantPares;
    }
 
    public void run() {
       for (int i = id; i < array.length; i += NThreads) {
          if(array[i]%2 == 0){
-            this.quantEven++;
+            this.quantPares++;
          }
       }
    }
@@ -62,27 +61,25 @@ public class Arrayzada {
          NThreads = DIM;
       }
 
-      // Criando o array de threads
+      // Criando o array de threads e instancia
       T[] threads = new T[NThreads];
 
-      // Criando Instancia
       ArrayCompartilhado array = new ArrayCompartilhado(DIM);
 
-      // Criando Threads
+      // Criando e executando as threads
       for (int i=0; i < threads.length; i++) {
          threads[i] = new T(i, array, NThreads);
       }
 
-      // Executando as Threads
       for (int i=0; i<threads.length; i++) {
          threads[i].start();
       }
-      int quantEven=0;
+      int quantPares=0;
       // Aguardando o termino de Threads
       for (int i=0; i < threads.length; i++) {
          try {
             threads[i].join();
-            quantEven+=threads[i].getEven();
+            quantPares+=threads[i].getEven();
          } catch (InterruptedException e) {
             return;
          }
@@ -96,14 +93,15 @@ public class Arrayzada {
             validacao++;
          }
       }
-      if(quantEven!=validacao){
+      if(quantPares!=validacao){
          System.out.println("Erro de validacao");
          return;
       }
 
 
       System.out.println("A funcao foi validada com sucesso");
-      System.out.println("O numero de pares e igual a "+quantEven);
+      System.out.println("O numero de pares e igual a "+quantPares);
+
    }
 
 }
